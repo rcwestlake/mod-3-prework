2ae0a3a618346a1ca19db5861f8c336531cbc717
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-22 12:52:25 -0700

    Error w/ 9, will continue

---
+++ promises/9catcherror.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('es6-promise');
+
+function parsePromised(json) {
+  return new Promise(function(fulfill, reject) {
+    try {
+      fulfill(JSON.parse(json));
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
+parsePromised(process.argv[2])
+  .then(null, console.log);

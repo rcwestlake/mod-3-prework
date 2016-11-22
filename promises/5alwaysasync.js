11690cbdb0253d7adabee413a45cc5ceee2e9805
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-22 11:33:55 -0700

    Finish 5

---
+++ promises/5alwaysasync.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('es6-promise');
+
+const promise = new Promise(function(fulfill, reject){
+  fulfill('PROMISE VALUE');
+});
+
+promise.then(console.log);
+
+console.log('MAIN PROGRAM');
